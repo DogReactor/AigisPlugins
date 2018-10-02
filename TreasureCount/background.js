c3820c3678833394d8a81f19321961a69f6dc0d2
DeltAlice <Etalice@outlook.com>
2018-10-03 01:01:04 +0800

    feat(hunter) add prob check mechanism

--- TreasureCount/background.js
+++ TreasureCount/background.js
@@ -1,3 +1,5 @@
+const { ItemList, treasureHunter } = require('./item')
+
 var cargo = {
   UnitsList: {},
   NameText: [],
@@ -9,7 +11,7 @@ var cargo = {
   checkReady(post) {
     let flag = Object.values(this.DataReady).every(e => e)
     if (flag && this.isRequired) {
-      post.sendMessage({title:'UnitsInfo',data:this})
+      post.sendMessage({ title: 'UnitsInfo', data: this })
       this.isRequired = false
     }
     return flag
@@ -17,26 +19,28 @@ var cargo = {
 }
 
 var library = {
-  QuestsList:{},
-  MapsInfo:{}
+  QuestsList: {},
+  MapsInfo: {}
 }
-
 var spoilsHistory = []
 var mailBox = null
+
+var batman = null
+
 function run(pluginHelper) {
   mailBox = pluginHelper
   mailBox.onMessage((msg, sendResponse) => {
     switch (msg) {
       case 'Request units info':
         if (cargo.checkReady()) {
-          sendResponse({title:'UnitsInfo',data:cargo})
+          sendResponse({ title: 'UnitsInfo', data: cargo })
         } else {
           sendResponse('Wait to ready')
           cargo.isRequired = true
         }
         break
       case 'Request without promise':
-        sendResponse({title:'UnitsInfo',data:cargo})
+        sendResponse({ title: 'UnitsInfo', data: cargo })
         break
       case 'Request spoils':
         sendResponse(spoilsHistory)
@@ -46,13 +50,48 @@ function run(pluginHelper) {
     }
   })
 
+
+  subscribeDataChunk(pluginHelper)
+  subscribeUnitChangeEvent(pluginHelper)
+
+  pluginHelper.aigisGameDataService.subscribe('quest-start', (data, url) => {
+    console.log('quest-req', data)
+    batman = data.BL
+  }, true)
+  pluginHelper.aigisGameDataService.subscribe('quest-start', (data, url) => {
+    console.log('quest-response', data)
+    Promise.all([parseSpoils(data), treasureHunter.ModTeamProb(batman)])
+      .then(([dropInfo, sucMsg]) => {
+        console.log(dropInfo, sucMsg)
+        spoilsHistory.push(dropInfo)
+        mailBox.sendMessage({ title: 'updateSpoils', data: dropInfo })
+        return treasureHunter.RemarkProb(dropInfo)
+      }, rej => { throw rej })
+      .then(record => sendRecord(record)).catch(err => console.log(err))
+  })
+}
+
+function subscribeDataChunk(pluginHelper) {
   pluginHelper.aigisGameDataService.subscribe('allcards-info', (data, url) => {
     cargo.UnitsList = data
+    treasureHunter.LoadRawData('UnitsList', data)
     cargo.DataReady.UnitsList = true
     cargo.checkReady(mailBox)
   })
+  pluginHelper.aigisGameDataService.subscribe('allunits-info', (data, url) => {
+    treasureHunter.LoadRawData('BarrackInfo', data)
+  })
+  pluginHelper.aigisGameDataService.subscribe('AbilityConfig.atb', (data, url) => {
+    treasureHunter.LoadRawData('AbilityConfig', data.Contents)
+  })
+  pluginHelper.aigisGameDataService.subscribe('AbilityList.atb', (data, url) => {
+    treasureHunter.LoadRawData('AbilityList', data.Contents)
+  })
+  pluginHelper.aigisGameDataService.subscribe('PlayerUnitTable.aar', (data, url) => {
+    treasureHunter.LoadRawData('ClassInfo', data.Files[1].Content.Contents)
+  })
   pluginHelper.aigisGameDataService.subscribe('all-quest-info', (data, url) => {
-    console.log(data)
+    console.log('quest', data)
     library.QuestsList = data
   })
   pluginHelper.aigisGameDataService.subscribe('NameText.atb', (data, url) => {
@@ -71,38 +110,53 @@ function run(pluginHelper) {
           library.MapsInfo[data.Label][e.Name] = e.Content.Contents
         }
       })
-      console.log(library.MapsInfo)
     })
-
-  pluginHelper.aigisGameDataService.subscribe('quest-start', (data, url) => {
-    let thisDrop = parseSpoils(data)
-    spoilsHistory.push(thisDrop)
-    mailBox.sendMessage({title:'updateSpoils', data:thisDrop})
+}
+function subscribeUnitChangeEvent(pluginHelper) {
+  pluginHelper.aigisGameDataService.subscribe('new-gacha-result', (data, url) => {
+    treasureHunter.updateHunter('new-unit', data.PPU)
+  })
+  pluginHelper.aigisGameDataService.subscribe('white-guarantee-gacha', (data, url) => {
+    treasureHunter.updateHunter('new-unit', data.PPU)
+  })
+  pluginHelper.aigisGameDataService.subscribe('fame-gacha-result', (data, url) => {
+    treasureHunter.updateHunter('new-unit', data.PPU)
   })
 
+  pluginHelper.aigisGameDataService.subscribe('cc', (data, url) => {
+    treasureHunter.updateHunter('unit-evo', data.PPU)
+  })
+  pluginHelper.aigisGameDataService.subscribe('aw1', (data, url) => {
+    treasureHunter.updateHunter('unit-evo', data.PPU)
+  })
+  pluginHelper.aigisGameDataService.subscribe('aw2', (data, url) => {
+    treasureHunter.updateHunter('unit-evo', data.PPU)
+  })
+  pluginHelper.aigisGameDataService.subscribe('unit-sell', (data, url) => {
+    console.log(data)
+    treasureHunter.updateHunter('del-unit', data.PPU)
+  }, true)
 }
-
-function parseSpoils(data) {
-  let index = library.QuestsList.QuestID.findIndex(q=>q==data.QR.QuestID)
+function sendRecord(record) { }
+async function parseSpoils(data) {
+  let index = library.QuestsList.QuestID.findIndex(q => q == data.QR.QuestID)
   let mapKey = 'Map' + library.QuestsList.MapNo[index] + '.aar'
-  let entryKey = 'Entry' + library.QuestsList.EntryNo[index].toString().padStart(2,'0') + '.atb'
+  let entryKey = 'Entry' + library.QuestsList.EntryNo[index].toString().padStart(2, '0') + '.atb'
 
   let mapInfo = library.MapsInfo[mapKey][entryKey]
   let dropInfo = []
-  mapInfo.forEach((e,i)=>{
-    if(e.PrizeCardID>0) {
-      dropInfo.push({Treasure:e.PrizeCardID, ID:library.QuestsList.QuestID[index], Order:i})
+  mapInfo.forEach((e, i) => {
+    if (e.PrizeCardID > 0) {
+      dropInfo.push({ Treasure: e.PrizeCardID, ID: library.QuestsList.QuestID[index], Order: i })
     }
   })
-  assert(dropInfo.length===data.LOTTERY.Result.length)
+
   for (let i in dropInfo.length) {
     dropInfo[i].Num = data.LOTTERY.Result[i]
     let treasureKey = 'Treasure' + dropInfo[i].Treasure
     dropInfo[i].Treasure = library.QuestsList[treasureKey][index]
   }
-
-  return dropInfo
-
+  return Promise.resolve(dropInfo)
 }
 
 
--- TreasureCount/item.js
+++ TreasureCount/item.js
@@ -79,7 +79,121 @@ const items = [
     77
 ]
 
+class Hunter {
+    constructor(prob, target) {
+        this.ProbMod = prob
+        this.PreyFlag = target
+    }
+}
+var treasureHunter = {
+    referenceData: {
+        UnitsList: null,
+        BarrackInfo: null,
+        ClassInfo: null,
+        AbilityConfig: null,
+        AbilityList: null
+    },
+    registered:false,
+    Hunter: {
+        57: new Hunter(0, (obj) => obj >= 1001 && obj <= 1004),
+        58: new Hunter(0, (obj) => obj >= 1005 && obj <= 1008),
+        59: new Hunter(0, (obj) => obj >= 2001 && obj <= 2010),
+        60: new Hunter(0, (obj) => obj == 77 || obj == 133 || obj == 250 || obj == 320),
+        61: new Hunter(0, (obj) => [54, 55, 56, 57, 58, 59, 60, 136, 234, 290, 303, 333, 334, 335, 383, 384, 397, 433, 459, 491].indexOf(obj) != -1),
+        79: new Hunter(0, (obj) => this.referenceData.UnitsList.Rare[obj - 1] == 2),
+        80: new Hunter(0, (obj) => obj >= 1000)
+    },
+    LoadRawData(label, data) {
+        if(label === 'AbilityConfig') {
+            let i = 0
+            let configId = 0
+            this.referenceData.AbilityConfig = []
+            data.forEach(c=>{
+                if(c._ConfigID!=0){
+                    configId = c._ConfigID
+                    this.referenceData.AbilityConfig[configId] = []
+                }
+                this.referenceData.AbilityConfig[configId].push(
+                    {
+                        _InfluenceType:c._InfluenceType,
+                        _Param1:c._Param1
+                    }
+                )
+            })
+            console.log(label,this.referenceData.AbilityConfig)
+        } else {
+            console.log(label, data)
+            this.referenceData[label] = data
+        }
+        if (Object.values(this.referenceData).every(e => e !== null)) {
+            this.registerHunter()
+        }
+    },
+    registerHunter() {
+        this.referenceData.BarrackInfo.forEach((unit,index) => {
+            this.checkIfHunter(unit)
+        });
+        this.registered = true
+    },
+    checkIfHunter(unit) {
+        let i = unit.A1 - 1
+        let AbilityId = this.referenceData.UnitsList.Ability[i]
+        let classObj = this.referenceData.ClassInfo.find(cl => cl.ClassID === unit.A2)
+        if (classObj.MaxLevel < 99) {
+            AbilityId = this.referenceData.UnitsList.Ability_Default[i]
+        }
+        if (AbilityId < this.referenceData.AbilityList.length) {
+            let configId = this.referenceData.AbilityList[AbilityId]._ConfigID
+            if(configId!=0){
+                this.referenceData.AbilityConfig[configId].forEach(c=>{
+                    if (c._InfluenceType!=80&&this.Hunter.hasOwnProperty(c._InfluenceType)) {
+                        this.Hunter[c._InfluenceType].ProbMod = Math.max(this.Hunter[c._InfluenceType].ProbMod, c._Param1)
+                    }
+                })
+            } 
+        }
+    },
+    updateHunter(operation, unit){
+        switch(operation) {
+            case 'unit-evo':
+            let member = this.BarrackInfo.find(u=>u.A7===unit.A7)
+            member.A2=unit.A2
+            this.checkIfHunter(member)
+            break
+            case 'new-unit':
+            this.BarrackInfo.push(unit)
+            this.checkIfHunter(unit)
+            break
+            case 'del-unit':
+            break
+            default:break
+        }
+    },
+    async ModTeamProb(team) {
+        if(team&&this.registered) {
+            team.forEach(u=>{
+                let teammate = this.referenceData.BarrackInfo.find(e=>e.A7===u.A7)
+                let configId = this.referenceData.ClassInfo.find(cl => cl.ClassID === teammate.A2).ClassAbility1
+                console.log(configId,teammat)
+                if(configId>0&&configId<this.referenceData.AbilityConfig.length) {
+                    this.referenceData.AbilityConfig[configId].forEach(c=>{
+                        if (this.Hunter.hasOwnProperty(c._InfluenceType)) {
+                            this.Hunter[c._InfluenceType].ProbMod = Math.max(this.Hunter[c._InfluenceType].ProbMod, c._Param1)
+                        }
+                    })
+                } 
+            })
+            return Promise.resolve('Ok')   
+        } else {
+            return Promise.reject('Haven\'t got team or registered')
+        }     
+    },
+    async RemarkProb(dropInfo) {
+
+    }
+}
 
 module.exports = {
-    ItemList: new Set(items)
+    ItemList: new Set(items),
+    treasureHunter: treasureHunter
 }
